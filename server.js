890e475bd476eef6daf74777b5d78f4dcd2bcdbe
Lucas Rolim <lucasrolim142@gmail.com>
2026-04-24 10:54:44 -0300

    Correção da PORT

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use('/users', userRoutes);
 app.use('/tenants',tenantRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('servidor rodando na porta $(PORT)'));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
